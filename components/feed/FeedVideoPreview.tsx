328fdfc6b7d61277e31fbff2b62449cb358ff1e7
clubandplayer-arch <clubandplayer@gmail.com>
2026-02-16 11:19:27 +0000

    fix(mobile): remove invalid posterSource prop from VideoView

--- components/feed/FeedVideoPreview.tsx
+++ components/feed/FeedVideoPreview.tsx
@@ -1,17 +1,11 @@
 import { Image, View } from "react-native";
-import { VideoView, useVideoPlayer } from "expo-video";
 
 type FeedVideoPreviewProps = {
   uri: string;
   posterUri?: string | null;
 };
 
-export default function FeedVideoPreview({ uri, posterUri }: FeedVideoPreviewProps) {
-  const player = useVideoPlayer({ uri }, (videoPlayer) => {
-    videoPlayer.muted = true;
-    videoPlayer.pause();
-  });
-
+export default function FeedVideoPreview({ posterUri }: FeedVideoPreviewProps) {
   return (
     <View
       style={{
@@ -23,22 +17,8 @@ export default function FeedVideoPreview({ uri, posterUri }: FeedVideoPreviewPro
       }}
     >
       {posterUri ? (
-        <Image
-          source={{ uri: posterUri }}
-          style={{ position: "absolute", left: 0, top: 0, right: 0, bottom: 0 }}
-          resizeMode="cover"
-        />
+        <Image source={{ uri: posterUri }} style={{ width: "100%", height: "100%" }} resizeMode="cover" />
       ) : null}
-
-      {/* Il parent gestisce il tap (apre lightbox). Qui disabilitiamo solo l'interazione sul video */}
-      <View pointerEvents="none" style={{ width: "100%", height: "100%" }}>
-        <VideoView
-          player={player}
-          style={{ width: "100%", height: "100%" }}
-          nativeControls={false}
-          contentFit="contain"
-        />
-      </View>
     </View>
   );
 }
