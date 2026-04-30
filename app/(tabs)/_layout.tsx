8028d27bbd4e0209d996c8a6d1cd23eaba109bad
clubandplayer-arch <clubandplayer@gmail.com>
2026-04-30 12:00:32 +0200

    fix: default startup to feed and bump app version 3.0.0 (#240)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -358,6 +358,7 @@ export default function TabsLayout() {
 
       {/* TABS CONTENT */}
       <Tabs
+        initialRouteName="feed/index"
         screenOptions={{
           headerShown: false,
           tabBarHideOnKeyboard: true,
