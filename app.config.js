f399dbb75135e80af555e1d09804db7dafcb6b69
clubandplayer-arch <clubandplayer@gmail.com>
2026-05-06 18:54:28 +0200

    fix: update app.config.js (#255)

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ const config = {
   name: "Club & Player",
   slug: "clubandplayer",
   scheme: "clubandplayer",
-  version: "3.0.5",
+  version: "3.0.6",
   orientation: "portrait",
   userInterfaceStyle: "automatic",
 
@@ -22,7 +22,7 @@ const config = {
   ios: {
     supportsTablet: false,
     bundleIdentifier: "com.clubandplayer.app",
-    buildNumber: "45",
+    buildNumber: "46",
     infoPlist: {
       NSPhotoLibraryUsageDescription:
         "Club & Player accede alle tue foto per permetterti di caricare immagini nei post, nel profilo e nelle opportunità sportive.",
@@ -34,7 +34,7 @@ const config = {
       android: {
         package: "com.clubandplayer.app",
         googleServicesFile: "./google-services.json",
-        versionCode: 45,
+        versionCode: 46,
         intentFilters: [
       {
         action: "VIEW",
